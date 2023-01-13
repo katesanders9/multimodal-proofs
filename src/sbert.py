da6651c9ba1d55db9c2bf0483f79971f4cae728d
katesanders9 <katesandersx@gmail.com>
2023-01-13 17:25:00 -0500

    SBERT edits

--- src/sbert.py
+++ src/sbert.py
@@ -1,9 +1,10 @@
+import json
 import random
 from collections import defaultdict
 from sentence_transformers import SentenceTransformer, SentencesDataset
 from sentence_transformers.losses import TripletLoss
 from sentence_transformers.readers import LabelSentenceReader, InputExample
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 TRAIN_JSON = '/srv/local2/ksande25/NS_data/TVQA/sbert_data_filtered.jsonl'
 
@@ -23,7 +24,7 @@ class Triplets(Dataset):
          
     def __len__(self):
         if self.is_train:
-            return len(self.chunks) + len(self.dialogue_chunks)
+            return len(self.chunks)
         else:
             return len(self.chunks)
     
@@ -42,21 +43,23 @@ class Triplets(Dataset):
             negative_list = self.dialogue_chunks[str(neg_id)]
             negative = random.choice(negative_list)
             
-            return anchor['h'], positive['text'], negative['text']
+            return InputExample(texts=[anchor['h'], positive['text'], negative['text']])
         
         else:
-            return anchor['text']
+            return InputExample(texts=[anchor['text']])
 
 def preprocess(data_file, train):
     data = []
-    for line in open(data_file, 'r'):
-        data.append(json.loads(line))
+    with open(data_file, 'r') as f:
+        data = json.load(f)
     return data
 
-
-sbert_model = SentenceTransformer('all-mpnet-base-v2')
+print("Loading data...")
 data = Triplets(TRAIN_JSON, train=True)
-dataset = DataLoader(data, batch_size=batch_size, shuffle=True)
-
+dataset = DataLoader(data, batch_size=32, shuffle=True)
+print("Loading model...")
+sbert_model = SentenceTransformer('all-mpnet-base-v2')
 loss = TripletLoss(model=sbert_model)
-sbert_model.fit(train_objectives=[(dataset, loss)], epochs=5,output_path='sbert_tvqa')
+
+print("Training model...")
+sbert_model.fit(train_objectives=[(dataset, loss)], epochs=5,output_path='sbert_tvqa', show_progress_bar=True)
